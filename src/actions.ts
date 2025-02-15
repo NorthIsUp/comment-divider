3214c793d2fac17754a676172b534a54dd2f6f3c
Adam Hitchcock <adam@northisup.com>
2025-02-14 22:11:17 -0800

    cleanup

--- src/actions.ts
+++ src/actions.ts
@@ -10,21 +10,23 @@ const insertDividerAction: Action = (type, lang) => {
 
   const lines: TextLine[] = [];
   for (const selection of editor.selections) {
-    if (!selection.isSingleLine) continue;
+    // check that each selection is a single line
+    if (!selection.isSingleLine) throw new Error('MULTI_LINE');
 
+    // get the line at the active position of the selection
     const line = editor.document.lineAt(selection.active.line);
-    lines.push(line);
-  }
 
-  if (type === 'mainHeader' || type === 'subheader') {
-    for (const line of lines) {
-      checkEmptyLine(line);
-    }
+    // check that the line is not empty if the preset is mainHeader or subheader
+    if (type === 'mainHeader' || type === 'subheader') checkEmptyLine(line);
+
+    // add the line to the lines array
+    lines.push(line);
   }
 
   window.activeTextEditor
     .edit((textEditorEdit: TextEditorEdit) => {
       for (const line of lines) {
+        // render the content and replace the line with the rendered content
         const content = render(type, line.text, lang);
         textEditorEdit.replace(line.range, content);
       }
