9e3a9f148a64cdf26385519b002430b1797d6b9d
Francesco Di Nucci <d1nuc0m@protonmail.com>
2025-12-23 18:38:25 +0100

    style: sort languages

--- src/limiters.ts
+++ src/limiters.ts
@@ -20,16 +20,18 @@ const getLanguageDefaultLimiters = (lang?: string): ILimiters => {
     case 'php':
     case 'sass':
     case 'scala':
-    case 'stylus':
     case 'sql':
+    case 'stylus':
     case 'swift':
     case 'typescript':
     case 'typescriptreact':
       return wrapLimiters('/*', '*/');
 
     case 'bash':
-    case 'dockerfile':
     case 'coffeescript':
+    case 'dockerfile':
+    case 'gdscript':
+    case 'home-assistant':
     case 'ignore':
     case 'julia':
     case 'makefile':
@@ -44,8 +46,6 @@ const getLanguageDefaultLimiters = (lang?: string): ILimiters => {
     case 'shellscript':
     case 'yaml':
     case 'yml':
-    case 'home-assistant':
-    case 'gdscript':
       return wrapLimiters('#', '#');
 
     case 'html':
@@ -57,10 +57,10 @@ const getLanguageDefaultLimiters = (lang?: string): ILimiters => {
       return wrapLimiters('<!--', '-->');
 
     case 'clojure':
-    case 'lisp':
-    case 'scheme':
     case 'ini':
+    case 'lisp':
     case 'rainmeter':
+    case 'scheme':
       return wrapLimiters(';', ';');
 
     case 'elm':
